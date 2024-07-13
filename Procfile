a18bef355de0c5456d2fcdce56426848c3a23e91
Fortune Linus <84579763+Dfortune014@users.noreply.github.com>
2024-07-13 18:31:07 -0500

    Add root package.json and concurrently setup

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: cd frontend && npm install --force && npm run build && npm run serve
+api: cd backend && npm install --force && node server.js
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 5173,
+    host: '0.0.0.0',  // This ensures the server is accessible from external IPs
+  },
   build: {
     outDir: 'dist',  // Ensure this is set to 'dist'
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // Define custom chunking if necessary
-        }
-      }
-    },
-    chunkSizeWarningLimit: 500,  // Adjust chunk size limit if necessary
   },
 })
