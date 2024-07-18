406c35162567b1feb1d866ca1c906dc4d60fa5ab
Fortune Linus <84579763+Dfortune014@users.noreply.github.com>
2024-07-18 17:15:08 -0500

    fixed errors

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import morgan from "morgan";
 import appRouter from "./routes/index.js";
-import openaiRouter from "./routes/openairoutes.js";
+import openaiRouter from "./routes/openAiRoutes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
